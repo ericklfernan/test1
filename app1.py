4a031ad98047563dfe56248f38d8ba095604b0f1
ericklfernan <47428559+ericklfernan@users.noreply.github.com>
2024-01-25 23:34:18 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,6 +9,20 @@ row_list = [["SNo", "Name", "Subject"],
               [2, "Gary Oak", "Mathematics"],
               [3, "Brock Lesner", "Physics"]]
 
+print(1, curr_path)
+print(2, curr_time)
+
+env_var1 = os.environ.get('ENV_VAR1', 'Unknown')
+print(3, env_var1)
+
+env_var2 = os.environ.get('ENV_VAR2', 'Unknown')
+print(4, env_var2)
+
+env_var3 = os.environ.get('ENV_VAR3', 'Unknown')
+print(5, env_var3)
+
 with open(f"{curr_path}/{curr_time}.csv","w",newline="") as file:
      writer = csv.writer(file)
      writer.writerows(row_list)
+
+print(6, 'Complete')
